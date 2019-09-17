da3ee0f09692011050c85397b3fdbb9dd6baaf2c
Jayant Mehta <jay9mehta@gmail.com>
2019-09-17 14:57:10 -0400

    update

--- src/components/homeStuff/Intro.vue
+++ src/components/homeStuff/Intro.vue
@@ -2,9 +2,11 @@
     <div>
         <div id="introWords">
             <h1> Hi, I'm {{myName.first}}!</h1>
-            <p>
-                I am a software developer studying Engineering at the University of Waterloo. Feel free to explore my website to get to know me :)
-            </p>
+            <vue-typer 
+                text="I am a software developer studying Engineering at the University of Waterloo. Feel free to explore my website to get to know me :)"
+                type-delay = '60'
+                :repeat='0'>
+            </vue-typer>
         </div>
         <a href="../../../static/JayantMehtaResume.pdf">Here is my Resume</a>
         <div id="introAccounts">
@@ -48,8 +50,8 @@ h1 {
     font-weight: bold;
 }
 
-p {
-  font-size: 23px;
+.vue-typer {
+  font-size: 20px;
 }
 
 a {
